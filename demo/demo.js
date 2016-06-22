02d411e779ed6a842a2e090f374fbe90e409bbb7
Chris Heninger <heninger@gmail.com>
2016-06-22 16:49:25 -0700

    Move load/export data from SynapseDraft component to demo page, still TODO on implementation 🔨

--- demo/demo.js
+++ demo/demo.js
@@ -3,6 +3,15 @@ import SynapseDraft from '../src/SynapseDraft';
 
 require('./demo.scss');
 
+/* eslint-disable */
+// TODO
+// const prebuiltRaw = {
+//   "entityMap":{},
+//   "blocks":[{"key":"au7ob","text":"RAW: Heading 1","type":"header-one","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"4ea5l","text":"And a blockquote","type":"blockquote","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"40cb9","text":"With a list","type":"unordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"6r9r7","text":"And some items","type":"unordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"6qt67","text":"Maybe another item","type":"unordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"2lu4p","text":"And a secondary heading","type":"header-two","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"90cfp","text":"With a paragraph at the end to wrap it all up","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[]}]
+// };
+// const contentStateFromRaw = convertFromRaw(prebuiltRaw);
+/* eslint-enable */
+
 function SynapseDraftDemo() {
   return (
     <div className="demo__wrapper">
@@ -14,6 +23,28 @@ function SynapseDraftDemo() {
         View this project on <a href="https://github.com/synapsestudios/synapse-draft">Github</a>
       </p>
       <SynapseDraft />
+      {/*
+        TODO
+        <div>
+          <button
+            onClick={() => {
+              this.setState({
+                editorState: EditorState.createWithContent(contentStateFromRaw),
+              });
+            }}
+          >
+            Load from Raw
+          </button>
+          <button
+            onClick={() => {
+              console.log(convertToRaw(contentState));
+              console.log(JSON.stringify(convertToRaw(contentState)));
+            }}
+          >
+            Convert To Raw
+          </button>
+        </div>
+      */}
     </div>
   );
 }
--- src/SynapseDraft.jsx
+++ src/SynapseDraft.jsx
@@ -4,17 +4,7 @@ import {
   Editor,
   EditorState,
   RichUtils,
-  convertToRaw,
-  convertFromRaw,
 } from 'draft-js';
-
-/* eslint-disable */
-const prebuiltRaw = {
-  "entityMap":{},
-  "blocks":[{"key":"au7ob","text":"RAW: Heading 1","type":"header-one","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"4ea5l","text":"And a blockquote","type":"blockquote","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"40cb9","text":"With a list","type":"unordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"6r9r7","text":"And some items","type":"unordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"6qt67","text":"Maybe another item","type":"unordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"2lu4p","text":"And a secondary heading","type":"header-two","depth":0,"inlineStyleRanges":[],"entityRanges":[]},{"key":"90cfp","text":"With a paragraph at the end to wrap it all up","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[]}]
-};
-const contentStateFromRaw = convertFromRaw(prebuiltRaw);
-/* eslint-enable */
 
 import BlockStyleControls from './BlockStyleControls';
 import InlineStyleControls from './InlineStyleControls';
@@ -77,45 +67,24 @@ class SynapseDraft extends Component {
     }
 
     return (
-      <div>
-        <div className="SynapseDraft-root">
-          <BlockStyleControls
+      <div className="SynapseDraft-root">
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={this.toggleBlockType}
+        />
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={this.toggleInlineStyle}
+        />
+        <div className={className} onClick={this.focus}>
+          <Editor
             editorState={editorState}
-            onToggle={this.toggleBlockType}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange}
+            placeholder="Tell a story..."
+            ref="editor"
+            spellCheck
           />
-          <InlineStyleControls
-            editorState={editorState}
-            onToggle={this.toggleInlineStyle}
-          />
-          <div className={className} onClick={this.focus}>
-            <Editor
-              editorState={editorState}
-              handleKeyCommand={this.handleKeyCommand}
-              onChange={this.onChange}
-              placeholder="Tell a story..."
-              ref="editor"
-              spellCheck
-            />
-          </div>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              this.setState({
-                editorState: EditorState.createWithContent(contentStateFromRaw),
-              });
-            }}
-          >
-            Load from Raw
-          </button>
-          <button
-            onClick={() => {
-              console.log(convertToRaw(contentState));
-              console.log(JSON.stringify(convertToRaw(contentState)));
-            }}
-          >
-            Convert To Raw
-          </button>
         </div>
       </div>
     );
